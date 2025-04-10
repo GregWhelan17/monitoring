e86a23e4fb345732d2071bb193cb4709a48b809c
turbo <turbo@turbo3.cluster.local>
2025-04-10 07:55:22 -0400

    runmonitor

---
+++ docker-image/scripts/runmonitor.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+echo 'Setting kubernets config'
+mkdir -p ~/.kube
+cp /kubeconfig/config ~/.kube/config
+
+echo 'getting configs and secrets'
+for f in /config/*/* ; do
+    case $(basename(${f})) in
+        noncriticalpods)
+            noncriticalpods=$(cat ${f})
+            ;;
+        scripts)
+            scripts=$(cat ${f})
+            ;;
+        turbohost)
+            turbohost=$(cat ${f})
+            ;;
+        turbopass)
+            turbopass=$(cat ${f})
+            ;;
+        turbouser)
+            turbouser=$(cat ${f})
+            ;;
+        *)
+            echo 'unexpected secret or configmap entry found'
+            ;;
+    esac
+done
+
+
+
+result=$(./monitor.sh -t turbohost -u turbouser -p turbopass -s scripts -n noncriticalpods)
+
+echo "$result"
